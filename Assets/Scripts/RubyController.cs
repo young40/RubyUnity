524a489e0cee51b234e22d98444842b16fa6f576
Peter Young <young40@gmail.com>
2020-02-09 00:03:51 +0800

    frame inpendent

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,7 +7,8 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+       // Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
@@ -18,8 +19,8 @@ public class RubyController : MonoBehaviour
         Debug.Log(horizontal);
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
